939b229c05c1ef109f9d2d73502227b03840a91d
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-04 00:13:12 -0300

    Update janela_custom_tkinter.py

--- janela_custom_tkinter.py
+++ janela_custom_tkinter.py
@@ -26,15 +26,15 @@ def clique():
     print("Fazer login")
 
 # escrevendo na janela 
-texto = customtkinter.CTkLabel(janela, text="Fazer Login")
+texto = customtkinter.CTkLabel(janela, text="Login", font=("Arial", 30))
 
 #colocando texto na janela e passando a distancia dos itens 
-texto.pack(padx=10, pady=10)
+texto.pack(padx=10, pady=40)
 
 #criando campo para digitar email e senha
-email = customtkinter.CTkEntry(janela, placeholder_text="Seu e-mail")
+email = customtkinter.CTkEntry(janela, placeholder_text="Seu e-mail",width=(200))
 email.pack(padx=10, pady=10)
-senha = customtkinter.CTkEntry(janela, placeholder_text="Sua senha", show="*")
+senha = customtkinter.CTkEntry(janela, placeholder_text="Sua senha", show="*", width=(200))
 senha.pack(padx=10, pady=10)
 
 #colocando a janela de chackbox 
@@ -42,7 +42,7 @@ checkbox = customtkinter.CTkCheckBox(janela, text="Lembrar Login")
 checkbox.pack(padx=10, pady=10)
 
 #colocando um botão 
-botao = customtkinter.CTkButton(janela, text="Login", command=clique)
+botao = customtkinter.CTkButton(janela, text="Login", command=clique, width=(200))
 botao.pack(padx=10, pady=10)
 
 #rodando a janela
